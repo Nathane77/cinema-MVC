fbd0ec27a529ee773f0a2d8ab600e86aea5e5dc4
Nathane77 <hamidoulila14@gmail.com>
2024-12-24 11:31:10 +0100

    added cross link, added css to category list

---
+++ controller/CategoryController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Controller;
+use Model\Connect;
+
+class CategoryController {
+
+    public function listCategories() {
+        
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->query("
+        SELECT g.genre_name, g.id_genre
+        FROM genre g
+        ");
+        
+        require "view/listCategories.php";
+
+    }
+
+    public function categoryDetails($id) {
+
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+        SELECT g.genre_name
+        FROM genre g
+        WHERE g.id_genre = :id
+        ");
+        
+        $requete->execute(["id"=>$id]);
+
+
+        $categoryDetails = $pdo->prepare("
+        SELECT f.film_title, f.id_film ,g.genre_name
+        FROM genre g
+        INNER JOIN film f ON f.film_genre = g.id_genre
+        WHERE g.id_genre = :id
+        ");
+
+        $categoryDetails->execute(["id"=>$id]);
+
+        require "view/categoryDetails.php";
+
+    }
+
+    
+}
